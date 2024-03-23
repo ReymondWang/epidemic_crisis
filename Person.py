c225dfc7905c36f11fec4bc22eb41d82c207f002
ZhangBennn <qq1240217513@icloud.com>
2024-03-23 20:54:36 +0800

    Add files via upload

--- Person.py
+++ Person.py
@@ -1,39 +1,14 @@
 from agentscope.agents import AgentBase
 from typing import Optional, Union, Any, Callable
-from enums import InfectionLevel, HealthLevel
-from Resource import Resource
-from Virus import Virus
 
-class Person(AgentBase):
+class Person(object):
     def __init__(
         self, 
         name: str, 
-        sys_prompt: str = None, 
-        model_config_name: str = None, 
-        infection: InfectionLevel = InfectionLevel.CLEAN, 
-        resource: Resource = None, 
-        virus: Virus = None, 
-        avatar: str = "",
-        uid: str = None
     ) -> None:
-        super().__init__(name, sys_prompt, model_config_name)
-        self.infection = infection
-        self.resource = resource
-        self.virus = virus
-        self.avatar = avatar
-        self.uid = uid
-        
-        self._init_status()
-    
-    def _init_status(self):
-        self.physicalHealth = HealthLevel.PERFECT
-        self.mindHealth = HealthLevel.PERFECT
-        self.isWearingMask = False
-        self.isDead = False
-        self.relation = []
-        
-    def die(self):
-        pass
+        super().__init__()
+        self.name = name
+        print("My name is %s" % self.name)
         
 if __name__ == "__main__":
     person = Person(name="reymond")
