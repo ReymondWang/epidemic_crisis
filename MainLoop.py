d4e04b5dc236d3ed8c88b4eed20c19288fbf84e3
ZhangBennn <qq1240217513@icloud.com>
2024-03-23 20:57:08 +0800

    增加查看状态功能

--- MainLoop.py
+++ MainLoop.py
@@ -1,5 +1,106 @@
+import time
+import json
+import inquirer
+import sys
+from SystemAgent import SystemAgent
+from Resource import Resource
+from Virus import Virus
+from Place import Place
+from Medicine import Medicine
+from Person import Person
+from enums import InfectionLevel, EffectLevel
 from utils import SYS_MSG_PREFIX
-from utils import send_chat_msg
+from utils import send_chat_msg, send_player_msg, send_player_input, get_player_input
+from agentscope.message import Msg
+
+#----定义药品相关的信息 start----
+medicine_status = {
+    "盘尼西林": "Y",
+    "奥斯他维": "N",
+    "RNA疫苗": "N",
+    "强力消毒液": "N"
+}
+
+penicillin = Medicine(
+    name="盘尼西林",
+    effect=EffectLevel.POOR,
+    price=2,
+    researchCnt=2
+)
+osweita = Medicine(
+    name="奥斯他维",
+    effect=EffectLevel.COMMON,
+    price=4,
+    researchCnt=4
+)
+rna = Medicine(
+    name="RNA疫苗",
+    effect=EffectLevel.GOOD,
+    price=6,
+    researchCnt=6
+)
+disinfection = Medicine(
+    name="强力消毒液",
+    effect=EffectLevel.GOOD,
+    price=6,
+    researchCnt=6
+)
+medicine_dic = {
+    "盘尼西林": penicillin,
+    "奥斯他维": osweita,
+    "RNA疫苗": rna,
+    "强力消毒液": disinfection,
+}
+
+#----定义药品相关的信息 end----
+
+def place_loop(place: Place, uid):
+    place.welcome()
+    msg = place.show_main_menu()
+    while True:
+        msg = place(msg)
+        if msg.get("content") == "***kill_virus***":
+            show_available_medicine(uid)
+        elif msg.get("content") == "***end***":
+            break
+    return Msg(
+            name=place.name,
+            role="user",
+            content="主菜单"
+        )
+
+
+def inspection_loop(person: Person, uid, SystemAgent):
+    person.self_introduction()
+    return SystemAgent.show_main_menu()
+
+def show_available_medicine(uid):
+    medicine_list = []
+    for key in medicine_status:
+        if medicine_status[key] == "Y":
+            medicine_list.append(key)
+    choose_medicine = f""" {SYS_MSG_PREFIX}请选择要使用: <select-box shape="card" 
+        type="checkbox" item-width="auto" 
+        options='{json.dumps(medicine_list, ensure_ascii=False)}' 
+        select-once></select-box>"""
+    send_chat_msg(
+        choose_medicine,
+        flushing=False,
+        uid=uid,
+    )
+    medicine = []
+    while True:
+        sel_medicine = get_player_input(uid=uid)
+        if isinstance(sel_medicine, str):
+            send_chat_msg(f" {SYS_MSG_PREFIX}请在列表中进行选择。", uid=uid)
+            continue
+        medicine = sel_medicine
+        break
+    send_chat_msg("**end_choosing**", uid=uid)
+    send_player_msg(msg=medicine[0], uid=uid)
+    
+    return Msg(name="user", content=medicine[0])
+            
 
 def main_loop(args) -> None:
     game_description = f"""
@@ -11,3 +112,155 @@ def main_loop(args) -> None:
     当所有的玩家、所有村民和场所都没有被感染时，即取得游戏的胜利。
     """
     send_chat_msg(game_description, uid=args.uid)
+    
+    round_menu_dict = {
+        "menu": ["查看状态", "研发药品", "采购物资", "与村民交谈"],
+        "inspection": ["自己", "小美", "花姐", "凯哥"],
+        "research": ["盘尼西林", "奥斯他维", "RNA疫苗", "强力消毒液"],
+        "place": ["百货商场", "大药房", "医院"],
+        "talking": ["小美", "花姐", "凯哥"]
+    }
+    
+    #----定义病毒相关的信息 start----
+    place_infection_feature = {
+        InfectionLevel.CLEAN: "无任何病毒，零污染。",
+        InfectionLevel.TINY: "存在少量新冠病毒污染，无明显特征。",
+        InfectionLevel.COMMON: "存在病毒，有少量人员出现发热、干咳、乏力、肌肉或关节疼痛、喉咙痛、失去嗅觉或味觉。",
+        InfectionLevel.SERIOUS: "存在较多病毒，有较多人员出现发热、干咳等症状，有少量人员症状严重。",
+        InfectionLevel.CRITICAL: "存在大量病毒，有很多症状严重的人员，甚至有人死亡。",
+        InfectionLevel.DEAD: "大量人员死亡，简直是人间地域。"
+    }
+    people_infection_feature = {
+        InfectionLevel.CLEAN: "完全没有感染，非常健康。",
+        InfectionLevel.TINY: "病毒潜伏期，没有明显症状或有非常轻微的症状，包括轻微乏力、肌肉酸痛、轻微的消化道症状（如恶心、腹泻）或者极轻微的发热等",
+        InfectionLevel.COMMON: "症状初发期，体温升高，通常是38℃以上，干咳，乏力或身体不适。",
+        InfectionLevel.SERIOUS: "症状严重期，呼吸困难，表现为呼吸急促、喘息、氧气饱和度下降等， 肺炎症状：胸痛、咳嗽加重，咳出黄绿色痰液，肺部影像学检查显示有肺炎样病变。",
+        InfectionLevel.CRITICAL: "症状非常严重，有多器官功能损害，包概括心肌损伤、肾功能异常、肝脏随让，神经系统并发症，并伴有休克、血压急剧降低、循环衰竭等。",
+        InfectionLevel.DEAD: "患者死亡"
+    }
+    virus_descption = f"""
+        新冠病毒（SARS-CoV-2），即导致COVID-19疾病的病毒，主要具有以下特征：
+        1、传播性强：新冠病毒具有高度传染性，能够在人际间快速传播，尤其在密闭、人员聚集的空间内传播风险更高。
+        传播途径包括但不限于呼吸道飞沫传播、接触传播（接触被病毒污染的物体表面再触摸口鼻眼）、以及可能存在的气溶胶传播。
+        2、潜伏期较长：感染后的潜伏期一般为1-14天，平均约为3-7天，有些病例潜伏期可能会更长，在潜伏期内就有可能传染给他人。
+        3、传染人群广泛：几乎所有年龄段的人都易感，尽管老年人和有基础疾病的人群更易发展为重症，但任何人都可能成为感染者。
+        随着疫苗接种的推进，已接种疫苗的人群即使感染，通常症状较轻或者无症状。
+        4、临床症状多样性：新冠病毒感染的症状可以从轻微到严重，轻症患者可能仅有轻微发热、乏力、干咳、喉咙痛、肌肉或关节疼痛、头痛、味觉或嗅觉丧失等。
+        而重症患者可能出现呼吸急促、低氧血症、肺炎、急性呼吸窘迫综合症、多器官功能障碍，甚至死亡。
+        """
+    
+    place_virus = Virus(
+        name="新冠病毒",
+        description=virus_descption,
+        feature_dict=place_infection_feature
+    )
+    people_virus = Virus(
+        name="新冠病毒",
+        description=virus_descption,
+        feature_dict=people_infection_feature
+    )
+    #----定义病毒相关的信息 end----
+    
+    #----系统Agent start----
+    systemAgent = SystemAgent(
+        name="小精灵", 
+        model_config_name="qwen_72b", 
+        sys_prompt="你是一个游戏的系统控制角色，负责推进游戏的进行，并且生成一些相关背景。",
+        round_menu_dict=round_menu_dict,
+        uid=args.uid
+    )
+    #----系统Agent end----
+    
+    #----场所Agent start----
+    place_menu_dict = {
+        "mall": ["采购食物", "病毒消杀", "结束"],
+        "pharmacy": ["采购口罩", "病毒消杀", "结束"],
+        "hospital": ["采购药品", "病毒消杀", "结束"],
+    }
+    
+    mall = Place(
+        name="百货商场",
+        model_config_name="qwen_72b",
+        sys_prompt="你是一个百货商场的经营人员，商场主要售卖各种食物，总是热情的欢迎所有客人。",
+        menu_list=place_menu_dict["mall"],
+        resource=Resource(food=sys.maxsize),
+        virus=place_virus,
+        avatar="./assets/mall.jpg",
+        uid=args.uid
+    )
+    pharmacy = Place(
+        name="大药房",
+        model_config_name="qwen_72b",
+        sys_prompt="你是一个药店的经营人员，药店主要售卖各种口罩，总是热情的欢迎所有来买药的人。",
+        menu_list=place_menu_dict["pharmacy"],
+        resource=Resource(mask=sys.maxsize),
+        virus=place_virus,
+        avatar="./assets/pharmacy.jpg",
+        uid=args.uid
+    )
+    hospital = Place(
+        name="医院",
+        model_config_name="qwen_72b",
+        sys_prompt="你是一个医院的导诊台护士，医院里可以买到各类药品，总是非常耐心的解答所有病人的问题。",
+        menu_list=place_menu_dict["hospital"],
+        resource=Resource(),
+        virus=place_virus,
+        avatar="./assets/hospital.jpg",
+        uid=args.uid
+    )
+    hospital.gen_resource(medicine_status)
+    
+    place_dic = {
+        "百货商场": mall,
+        "大药房": pharmacy,
+        "医院": hospital,
+    }
+    #----场所Agent end----
+    
+    #----人员Agent start----
+    beauty = Person(
+        name="小美",
+        model_config_name="qwen_72b",
+        sys_prompt="你是一个心地善良的女生，性格活泼开朗，曾经在百货商场上过一段时间班。",
+        resource=Resource(),
+        virus=people_virus,
+        avatar="./assets/npc1.jpg",
+        uid=args.uid
+    )
+    
+    flower = Person(
+        name="花姐",
+        model_config_name="qwen_72b",
+        sys_prompt="你是一个中年女性，随让脾气有些刻板，但是生活很幸福，曾经在大药房上过一段时间班。",
+        resource=Resource(),
+        virus=people_virus,
+        avatar="./assets/npc2.jpg",
+        uid=args.uid
+    )
+    
+    king = Person(
+        name="凯哥",
+        model_config_name="qwen_72b",
+        sys_prompt="你是一个中年男性，是一个竟然非常丰富的内科医生，曾经有一个女儿，但是很不幸两年前离婚了，女儿跟着妈妈走了。",
+        resource=Resource(),
+        virus=people_virus,
+        avatar="./assets/npc3.jpg",
+        uid=args.uid
+    )
+    
+    npc_dict = {
+        "小美": beauty,
+        "花姐": flower,
+        "凯哥": king
+    }
+    #----人员Agent end----
+    
+    msg = systemAgent.begin_new_round()
+    while True:
+        msg = systemAgent(msg)
+        content = msg.get("content")
+        if content in place_dic:
+            msg = place_loop(place_dic[content], uid=args.uid)
+
+        if content in npc_dict:
+            msg = inspection_loop(npc_dict[content], uid=args.uid, SystemAgent=systemAgent)
--- Person.py
+++ Person.py
@@ -1,15 +1,83 @@
 from agentscope.agents import AgentBase
 from typing import Optional, Union, Any, Callable
-
-class Person(object):
+from enums import InfectionLevel, HealthLevel
+from Resource import Resource
+from Virus import Virus
+from agentscope.message import Msg
+from utils import send_chat_msg
+from agentscope.prompt import PromptType, PromptEngine
+InfectionLevel_TEXT = {
+    0: "Clean",
+    1: "Tiny",
+    2: "Common",
+    3: "Serious",
+    4: "Critical",
+    5: "Dead"
+}
+HealthLevel_TEXT = {
+    5: "Perfect",
+    4: "Good",
+    3: "Common",
+    2: "Bad",
+    1: "Poor",
+    0: "Dead"
+}
+WearingMask_TEXT = {
+    True: "已佩戴",
+    False: "未佩戴"
+}
+class Person(AgentBase):
     def __init__(
         self, 
         name: str, 
+        sys_prompt: str = None, 
+        model_config_name: str = None, 
+        infection: InfectionLevel = InfectionLevel.CLEAN, 
+        resource: Resource = None, 
+        virus: Virus = None, 
+        avatar: str = "",
+        uid: str = None
     ) -> None:
-        super().__init__()
-        self.name = name
-        print("My name is %s" % self.name)
+        super().__init__(name, sys_prompt, model_config_name)
+        self.engine = PromptEngine(self.model, prompt_type=PromptType.LIST)
+        self.infection = infection
+        self.resource = resource
+        self.virus = virus
+        self.avatar = avatar
+        self.uid = uid
         
+        self._init_status()
+    
+    def _init_status(self):
+        self.physicalHealth = HealthLevel.PERFECT
+        self.mindHealth = HealthLevel.PERFECT
+        self.isWearingMask = False
+        self.isDead = False
+        self.relation = []
+        
+    def die(self):
+        pass
+    def self_introduction(self) -> Msg:
+        start_hint = "请生成一段自我介绍，100字以内。"
+
+        prompt = self.engine.join(
+            self.sys_prompt + start_hint,
+            self.memory.get_memory()
+        )
+
+        response = self.model(prompt, max=3)
+        information = (f"我的状态:\n"
+                       f"感染程度: {InfectionLevel_TEXT[self.infection]}\n"
+                       f"健康程度: {HealthLevel_TEXT[self.physicalHealth]}\n"
+                       f"精神状况: {HealthLevel_TEXT[self.mindHealth]}\n"
+                       f"佩戴口罩: {WearingMask_TEXT[self.isWearingMask]}\n"
+                       f"携带病毒: {'未携带' if self.infection == 0 else self.virus.name}\n"
+                       f"食物数量: {self.resource.get_food()}\n"
+                       f"口罩数量: {self.resource.get_mask()}\n"
+                       f"携带的药品: {self.resource.medicine}\n"
+                       )
+        msg = response.text + '\n' + information
+        send_chat_msg(msg, role=self.name, uid=self.uid, avatar=self.avatar)
 if __name__ == "__main__":
     person = Person(name="reymond")
     print(person.name)
